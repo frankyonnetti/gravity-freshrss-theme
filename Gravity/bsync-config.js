d42397df60b2d579f3bdcdc6f7a2a88e6c17c563
frankyonnetti <frank@designhammer.com>
2024-02-11 13:10:06 -0500

    Initial commit

---
+++ Gravity/bsync-config.js
@@ -0,0 +1,26 @@
+/*
+|--------------------------------------------------------------------------
+| Browser-sync config file
+|--------------------------------------------------------------------------
+|
+| For up-to-date information about the options:
+|   http://www.browsersync.io/docs/options/
+|
+| There are more options than you see here, these are just the ones that are
+| set internally. See the website for more info.
+*/
+
+module.exports = {
+  port: 3478,
+  proxy: 'local.freshrss.test',
+  open: false,
+  browser: 'google chrome',
+  ghostMode: false,
+  notify: true,
+  ui: false,
+  reloadOnRestart: true,
+  files: [
+    'gravity.css',
+    'gravity.rtl.css'
+  ]
+}
